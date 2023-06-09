705c33d20fdca258e298bf3242ebf26f16e42055
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-09 18:41:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let mainElement = document.getElementById("main");
+
+mainElement.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Nafie is the champion"; // Replace YOUR-NAME with your actual name
